1b191583a5c569db3ef9ad96c4c04eaaf7d3bf8a
Dmytro Hrynov <hrynovdmytro11@gmail.com>
2025-07-03 19:06:27 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="underline text-red-600 text-5xl font-bold">Hello World!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
